d0e939dc4a4a9c8e5b3f56e5d84f6d89c99d8235
MedNewton <med.newton.2013@gmail.com>
2023-09-27 02:05:30 +0100

    Fixed Trade page (mint/burn side)

--- components/minting.tsx
+++ components/minting.tsx
@@ -219,243 +219,258 @@ export default function Minting() {
             <PriceView />
           </TabPanel>
           <TabPanel>
-            <div className={styles.swapMintburnInner} />
-            <div className={styles.swapMintburnChild1} />
-            <div className={styles.antDesignswapOutlined}>
-              <div className={styles.max1}>max</div>
+            <div className="h-fit py-3 w-full">
+              <div className={styles.swapMintburnInner} />
+              <div className={styles.swapMintburnChild1} />
+              <div className={styles.antDesignswapOutlined}>
+                <div className={styles.max1}>max</div>
+              </div>
+
+              <Tabs variant="unstyled">
+                <TabList>
+                  <Tab
+                    style={thirdFont.style}
+                    className={styles.mint}
+                    _selected={{ color: "yellow", bg: "none" }}
+                  >
+                    <Image src={Mx} alt="" />
+                  </Tab>
+                  <Tab
+                    style={thirdFont.style}
+                    className={styles.burn}
+                    _selected={{ color: "yellow", bg: "none" }}
+                  >
+                    <Image src={Br} alt="" />
+                  </Tab>
+                </TabList>
+                <TabPanels>
+                  <TabPanel>
+                    <div className="credit">
+                      <Image
+                        className={styles.frameChild11}
+                        src={CommitLogo}
+                        alt={CommitLogo}
+                      />
+                      <Button
+                        style={thirdFont.style}
+                        sx={{ color: "black" }}
+                        onClick={handleClickOpen}
+                      >
+                        Buy Using Cards
+                      </Button>
+                      <Dialog open={open} onClose={handleClose}>
+                        <DialogContent>
+                          <Payment />
+                        </DialogContent>
+                      </Dialog>
+                    </div>
+
+                    <div className={styles.groupParent}>
+                      <Image
+                        className={styles.frameChild11}
+                        src={CommitLogo}
+                        alt={CommitLogo}
+                      />
+                      <button
+                        style={thirdFont.style}
+                        className={styles.g}
+                        onClick={handleMint}
+                      >
+                        <Image src={Commib} alt="" />
+                      </button>
+                    </div>
+
+                    <div className={styles.swapMintburnChild2} />
+
+                    <div
+                      style={{ fontFamily: "Inter" }}
+                      className={styles.youPay}
+                    >
+                      you pay
+                    </div>
+                    <div
+                      style={{ fontFamily: "Inter" }}
+                      className={styles.totalCost}
+                    >
+                      Total cost
+                    </div>
+                    <div className={styles.div2}>{amount}</div>
+                    <div
+                      style={{ fontFamily: "Inter" }}
+                      className={styles.expectedTokensMinted1}
+                    >
+                      Expected Tokens Minted
+                    </div>
+                    <div className={styles.div3}>{totalAmount}</div>
+                    <div
+                      style={{ fontFamily: "Inter" }}
+                      className={styles.expectedEquivalentExpousure1}
+                    >
+                      Expected Platform Fees
+                    </div>
+                    <div className={styles.div4}>0.2%</div>
+                    <div className={styles.frameDiv} />
+                    <div
+                      style={{ fontFamily: "Inter" }}
+                      className={styles.balance323400}
+                    >
+                      balance: $0
+                    </div>
+                    <div
+                      style={{ fontFamily: "Inter" }}
+                      className={styles.expectedPrice124681}
+                    >
+                      Expected Price: $124.68
+                    </div>
+                    <img
+                      className={styles.swapMintburnChild3}
+                      alt=""
+                      src="Vector 12.svg"
+                    />
+                    <img
+                      className={styles.swapMintburnChild4}
+                      alt=""
+                      src="Vector 13.svg"
+                    />
+                    <div className={styles.div5}>
+                      <input
+                        value={amount}
+                        placeholder=""
+                        className={styles.span1}
+                        style={{
+                          background: "none",
+                          width: "60%",
+                          borderRadius: "15px",
+                          border: "solid 1px #ffffff",
+                          fontSize: "18px",
+                          padding: "3% 3%",
+                        }}
+                        onChange={(e) => setAmount(e.target.value)}
+                      />
+                    </div>
+
+                    <div className={styles.frameGroup}>
+                      <div className={styles.ellipseParent}>
+                        <div
+                          style={{ fontFamily: "Inter" }}
+                          className={styles.g}
+                        >
+                          usdc
+                        </div>
+                      </div>
+                    </div>
+                  </TabPanel>
+                  <TabPanel>
+                    <div className={styles.groupParent}>
+                      <Image
+                        className={styles.frameChild11}
+                        src={CommitLogo}
+                        alt={CommitLogo}
+                      />
+                      <button
+                        style={{
+                          margin: "-4",
+                          fontFamily: "__thirdFont_3ea4f2",
+                          fontWeight: "900",
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "center",
+                        }}
+                        className={styles.g}
+                        onClick={handleBurn}
+                      >
+                        Commit Burn
+                      </button>
+                    </div>
+
+                    <div className={styles.swapMintburnChild2} />
+
+                    <div
+                      style={{ fontFamily: "Inter" }}
+                      className={styles.youPay}
+                    >
+                      you pay
+                    </div>
+                    <div
+                      style={{ fontFamily: "Inter" }}
+                      className={styles.totalCost}
+                    >
+                      Total cost
+                    </div>
+                    <div className={styles.div2}>{burnamount}</div>
+                    <div
+                      style={{ fontFamily: "Inter" }}
+                      className={styles.expectedTokensMinted1}
+                    >
+                      Expected Tokens Minted
+                    </div>
+                    <div className={styles.div3}>{totalBurnAmount}</div>
+                    <div
+                      style={{ fontFamily: "Inter" }}
+                      className={styles.expectedEquivalentExpousure1}
+                    >
+                      Expected Platform Fees
+                    </div>
+                    <div
+                      style={{ fontFamily: "Inter" }}
+                      className={styles.div4}
+                    >
+                      0.2%
+                    </div>
+                    <div className={styles.frameDiv} />
+                    <div
+                      style={{ fontFamily: "Inter" }}
+                      className={styles.balance323400}
+                    >
+                      balance: $0
+                    </div>
+                    <div
+                      style={{ fontFamily: "Inter" }}
+                      className={styles.expectedPrice124681}
+                    >
+                      Expected Price: $124.68
+                    </div>
+                    <img
+                      className={styles.swapMintburnChild3}
+                      alt=""
+                      src="Vector 12.svg"
+                    />
+                    <img
+                      className={styles.swapMintburnChild4}
+                      alt=""
+                      src="Vector 13.svg"
+                    />
+                    <div className={styles.div5}>
+                      <input
+                        value={burnamount}
+                        placeholder=""
+                        className={styles.span1}
+                        style={{
+                          background: "none",
+                          width: "60%",
+                          borderRadius: "15px",
+                          border: "solid 1px #ffffff",
+                          fontSize: "18px",
+                          padding: "3% 3%",
+                        }}
+                        onChange={(e) => setBurnamount(e.target.value)}
+                      />
+                    </div>
+
+                    <div className={styles.frameGroup}>
+                      <div className={styles.ellipseParent}>
+                        <div
+                          style={{ fontFamily: "Inter" }}
+                          className={styles.g}
+                        >
+                          tita
+                        </div>
+                      </div>
+                    </div>
+                  </TabPanel>
+                </TabPanels>
+              </Tabs>
             </div>
-
-            <Tabs variant="unstyled">
-              <TabList>
-                <Tab
-                  style={thirdFont.style}
-                  className={styles.mint}
-                  _selected={{ color: "yellow", bg: "none" }}
-                >
-                  <Image src={Mx} alt="" />
-                </Tab>
-                <Tab
-                  style={thirdFont.style}
-                  className={styles.burn}
-                  _selected={{ color: "yellow", bg: "none" }}
-                >
-                  <Image src={Br} alt="" />
-                </Tab>
-              </TabList>
-              <TabPanels>
-                <TabPanel>
-                  <div className="credit">
-                    <Image
-                      className={styles.frameChild11}
-                      src={CommitLogo}
-                      alt={CommitLogo}
-                    />
-                    <Button
-                      style={thirdFont.style}
-                      sx={{ color: "black" }}
-                      onClick={handleClickOpen}
-                    >
-                      Buy Using Cards
-                    </Button>
-                    <Dialog open={open} onClose={handleClose}>
-                      <DialogContent>
-                        <Payment />
-                      </DialogContent>
-                    </Dialog>
-                  </div>
-
-                  <div className={styles.groupParent}>
-                    <Image
-                      className={styles.frameChild11}
-                      src={CommitLogo}
-                      alt={CommitLogo}
-                    />
-                    <button
-                      style={thirdFont.style}
-                      className={styles.g}
-                      onClick={handleMint}
-                    >
-                      <Image src={Commib} alt="" />
-                    </button>
-                  </div>
-
-                  <div className={styles.swapMintburnChild2} />
-
-                  <div
-                    style={{ fontFamily: "Inter" }}
-                    className={styles.youPay}
-                  >
-                    you pay
-                  </div>
-                  <div
-                    style={{ fontFamily: "Inter" }}
-                    className={styles.totalCost}
-                  >
-                    Total cost
-                  </div>
-                  <div className={styles.div2}>{amount}</div>
-                  <div
-                    style={{ fontFamily: "Inter" }}
-                    className={styles.expectedTokensMinted1}
-                  >
-                    Expected Tokens Minted
-                  </div>
-                  <div className={styles.div3}>{totalAmount}</div>
-                  <div
-                    style={{ fontFamily: "Inter" }}
-                    className={styles.expectedEquivalentExpousure1}
-                  >
-                    Expected Platform Fees
-                  </div>
-                  <div className={styles.div4}>0.2%</div>
-                  <div className={styles.frameDiv} />
-                  <div
-                    style={{ fontFamily: "Inter" }}
-                    className={styles.balance323400}
-                  >
-                    balance: $0
-                  </div>
-                  <div
-                    style={{ fontFamily: "Inter" }}
-                    className={styles.expectedPrice124681}
-                  >
-                    Expected Price: $124.68
-                  </div>
-                  <img
-                    className={styles.swapMintburnChild3}
-                    alt=""
-                    src="Vector 12.svg"
-                  />
-                  <img
-                    className={styles.swapMintburnChild4}
-                    alt=""
-                    src="Vector 13.svg"
-                  />
-                  <div className={styles.div5}>
-                    $
-                    <input
-                      value={amount}
-                      placeholder=""
-                      className={styles.span1}
-                      style={{
-                        background: "none",
-                        width: "30%",
-                        border: "none",
-                      }}
-                      onChange={(e) => setAmount(e.target.value)}
-                    />
-                  </div>
-
-                  <div className={styles.frameGroup}>
-                    <div className={styles.ellipseParent}>
-                      <div style={{ fontFamily: "Inter" }} className={styles.g}>
-                        usdc
-                      </div>
-                    </div>
-                  </div>
-                </TabPanel>
-                <TabPanel>
-                  <div className={styles.groupParent}>
-                    <Image
-                      className={styles.frameChild11}
-                      src={CommitLogo}
-                      alt={CommitLogo}
-                    />
-                    <button
-                      style={{
-                        margin: "-4",
-                        fontFamily: "__thirdFont_3ea4f2",
-                        fontWeight: "900",
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                      }}
-                      className={styles.g}
-                      onClick={handleBurn}
-                    >
-                      Commit Burn
-                    </button>
-                  </div>
-
-                  <div className={styles.swapMintburnChild2} />
-
-                  <div
-                    style={{ fontFamily: "Inter" }}
-                    className={styles.youPay}
-                  >
-                    you pay
-                  </div>
-                  <div
-                    style={{ fontFamily: "Inter" }}
-                    className={styles.totalCost}
-                  >
-                    Total cost
-                  </div>
-                  <div className={styles.div2}>{burnamount}</div>
-                  <div
-                    style={{ fontFamily: "Inter" }}
-                    className={styles.expectedTokensMinted1}
-                  >
-                    Expected Tokens Minted
-                  </div>
-                  <div className={styles.div3}>{totalBurnAmount}</div>
-                  <div
-                    style={{ fontFamily: "Inter" }}
-                    className={styles.expectedEquivalentExpousure1}
-                  >
-                    Expected Platform Fees
-                  </div>
-                  <div style={{ fontFamily: "Inter" }} className={styles.div4}>
-                    0.2%
-                  </div>
-                  <div className={styles.frameDiv} />
-                  <div
-                    style={{ fontFamily: "Inter" }}
-                    className={styles.balance323400}
-                  >
-                    balance: $0
-                  </div>
-                  <div
-                    style={{ fontFamily: "Inter" }}
-                    className={styles.expectedPrice124681}
-                  >
-                    Expected Price: $124.68
-                  </div>
-                  <img
-                    className={styles.swapMintburnChild3}
-                    alt=""
-                    src="Vector 12.svg"
-                  />
-                  <img
-                    className={styles.swapMintburnChild4}
-                    alt=""
-                    src="Vector 13.svg"
-                  />
-                  <div className={styles.div5}>
-                    $
-                    <input
-                      value={burnamount}
-                      placeholder=""
-                      className={styles.span1}
-                      style={{
-                        background: "none",
-                        width: "30%",
-                        border: "none",
-                      }}
-                      onChange={(e) => setBurnamount(e.target.value)}
-                    />
-                  </div>
-
-                  <div className={styles.frameGroup}>
-                    <div className={styles.ellipseParent}>
-                      <div style={{ fontFamily: "Inter" }} className={styles.g}>
-                        tita
-                      </div>
-                    </div>
-                  </div>
-                </TabPanel>
-              </TabPanels>
-            </Tabs>
           </TabPanel>
         </TabPanels>
       </Tabs>
